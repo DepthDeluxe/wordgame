4f8e967d9127d31a0caac97a99841f1003489a5b
DepthDeluxe <cheinzmann3@gmail.com>
2014-04-13 15:08:35 -0400

    class improvements added a couple of extra functions, documented what the functions do

--- graph.py
+++ graph.py
@@ -9,6 +9,8 @@ class Vertex:
 			raise new Exception("Error: Words must be of length 5")
 
 	def connect(self, otherVertex, weight):
+		""" Connect this vertex to another with the given weight """
+
 		# configure this vertex's adjacency list and weight list
 		self.adj.append(otherVertex)
 		self.weights.append(weight)
@@ -17,14 +19,38 @@ class Vertex:
 		otherVertex.adj.append(self)
 		otherVertex.weights.append(weight)
 
+	def edgeExists(self, otherVertex):
+		""" Returns True if there is an edge between the two vertices """
+
+		# search the list for the vertex
+		for adjacent in self.adj:
+			if otherVertex == adjacent:
+				return adjacent
+
+		# return None if no edge exists
+		return None
+
+	def getWeight(self, otherVertex):
+		""" Returns the weight of the edge between two vertices """
+
+		# search the list for the vertex
+		for n in range(0, len(self.adj)):
+			if self.adj[n] == otherVertex:
+				return self.weights[n]
+
+		# return None if no edge exists
+		return None
+
 class Graph:
 	def __init__(self):
 		self.vertices = []
 
 	def addVertex(self, vertex):
+		""" Adds a vertex to the graph """
 		self.vertices.append(vertex)
 
 	def getVertexByWord(self, word):
+		""" Gets the vertex by the word that it represents """
 		# search through vertex list
 		for vertex in self.vertices:
 			if vertex.word == word:
